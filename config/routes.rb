768b09388f86c4284b2770c4e9da507bd6a568ec
HarrisLeow <leow_harris@hotmail.sg>
2016-06-27 08:52:51 +0800

    logout and sessions created

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   #I want all the routes except new routes
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
